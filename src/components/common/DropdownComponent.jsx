59ee6d7767edc1be1a11bcaf873156e0643f71b4
cesarbonadio <cesarbonadio123@gmail.com>
2025-01-30 10:16:25 -0400

    ✨; addded DropdownComponent to use in dataFiles

--- src/components/common/DropdownComponent.jsx
+++ src/components/common/DropdownComponent.jsx
@@ -1,37 +1,58 @@
 import React, { useState, useEffect } from "react";
 import { useFetchFilesList } from "../../hooks/useFetchFilesList";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const DropdownComponent = ({ onSelectionChange }) => {
-  const { data, isLoading, error } = useFetchFilesList();
-  const [selectedValue, setSelectedValue] = useState("");
+	const { data, isLoading, error } = useFetchFilesList();
+	const [selectedValue, setSelectedValue] = useState("");
 
-  const handleChange = (event) => {
-    const selectedOption = event.target.value;
-    setSelectedValue(selectedOption);
-    onSelectionChange(selectedOption);
-  };
+	const handleChange = (event) => {
+		const selectedOption = event.target.value;
+		setSelectedValue(selectedOption);
+		onSelectionChange(selectedOption);
+	};
 
-  useEffect(() => {}, [data]);
+	useEffect(() => {}, [data]);
 
-  return (
-    <div>
-      {isLoading ? (
-        <p>Cargando opciones...</p>
-      ) : error ? (
-        <p style={{ color: "red" }}>{error}</p>
-      ) : (
-        <Form.Select size="sm" value={selectedValue} onChange={handleChange}>
-          <option value="">Seleccione una opción</option>
-          {(data || {files: []})?.files.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </Form.Select >
-      )}
-    </div>
-  );
+	const handleClean = () => {
+		setSelectedValue("");
+		onSelectionChange("");
+	};
+
+	return (
+		<div>
+			{isLoading ? (
+				<p>Cargando opciones...</p>
+			) : error ? (
+				<p style={{ color: "red" }}>{error}</p>
+			) : (
+				<div style={{ display: "flex", justifyContent: "center" }}>
+					<Form.Select
+						style={{ width: "570px" }}
+						size="sm"
+						value={selectedValue}
+						onChange={handleChange}
+					>
+						<option value="">Seleccione una opción</option>
+						{(data || { files: [] })?.files.map((option) => (
+							<option key={option} value={option}>
+								{option}
+							</option>
+						))}
+					</Form.Select>
+					<Button
+						onClick={handleClean}
+						variant="outline-primary"
+						className="start-0 mx-0"
+					>
+						Limpiar
+					</Button>
+				</div>
+			)}
+		</div>
+	);
 };
 
 export default DropdownComponent;
